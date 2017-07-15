184e9ac4cdc787edf7c929e38a2cb231ee84e8fc
梁益彰 <elliot64@gmail.com>
2017-07-15 23:52:28 +0700

    no message

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/linebot');
+var linebot = require('linebot');
+ 
+var bot = linebot({
+    channelId: 1525063119,
+    channelSecret: 29ec9b2909480c86f7ccfd74a0f128f9,
+    channelAccessToken: sFWF5ufE+Ne9ST4/MnnZPCYl+TlyANM1cSYwJojmhqs3p59TH8NqKNThOYT/0fxkEpLkyc3uBLOwA7Sq4c0zVqbSzRAjLICB6I8hWhZ2NYUSSs0napNkKhkPOUckGSgkgllQnt+Mw6DgB9jj/IM4lwdB04t89/1O/w1cDnyilFU= 
+});
+ 
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+        // success 
+    }).catch(function (error) {
+        // error 
+    });
+});
+ 
+bot.listen('/linewebhook', 3000);
