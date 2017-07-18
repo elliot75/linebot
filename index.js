383dd22e1b0aaa83b9c1d2ee08a0a7b59f3a04df
elliot75 <elliot64@gmail.com>
2017-07-18 08:51:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-var linebot = require('./lib/linebot');
-//var express = require('express');
- 
-var bot = linebot({
-    channelId: "1525063119",
-    channelSecret: "29ec9b2909480c86f7ccfd74a0f128f9",
-    channelAccessToken: "sFWF5ufE+Ne9ST4/MnnZPCYl+TlyANM1cSYwJojmhqs3p59TH8NqKNThOYT/0fxkEpLkyc3uBLOwA7Sq4c0zVqbSzRAjLICB6I8hWhZ2NYUSSs0napNkKhkPOUckGSgkgllQnt+Mw6DgB9jj/IM4lwdB04t89/1O/w1cDnyilFU=" 
-});
- 
-bot.on('message', function (event) {
-    event.reply(event.message.text).then(function (data) {
-        // success 
-    }).catch(function (error) {
-        // error 
+var rp = require('request-promise');
+
+exports.justReply = function justReply(req, res) {
+
+    const promises = req.body.events.map(event => {
+
+        var msg = event.message.text;
+        var reply_token = event.replyToken;
+
+        var options = {
+            method: 'POST',
+            uri: "https://api.line.me/v2/bot/message/reply",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+              "Authorization": " Bearer " + "sFWF5ufE+Ne9ST4/MnnZPCYl+TlyANM1cSYwJojmhqs3p59TH8NqKNThOYT/0fxkEpLkyc3uBLOwA7Sq4c0zVqbSzRAjLICB6I8hWhZ2NYUSSs0napNkKhkPOUckGSgkgllQnt+Mw6DgB9jj/IM4lwdB04t89/1O/w1cDnyilFU="
+            },
+            json: true,
+            body: {
+              replyToken: reply_token,
+              messages:[
+                {
+                  "type":"text",
+                  "text":msg
+                }
+              ]
+            }
+        };
+
+        return rp(options)
+        .then(function (response) {
+            console.log("Success : " + response);
+        }).catch(function (err) {
+            console.log("Error : " + err);
+        });
+
     });
-});
- 
-bot.listen('/linewebhook', 3000);
+
+    Promise
+    .all(promises)
+    .then(() => res.json({success: true}));
+
+};
